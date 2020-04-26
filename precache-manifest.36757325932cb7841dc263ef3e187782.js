59d9e60c37ece5655b201f2749fd6104df8a6fc1
marklawrence97 <mark_lawrence97@icloud.com>
2020-04-26 13:25:31 +0100

    Updates

---
+++ precache-manifest.36757325932cb7841dc263ef3e187782.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eec0b2060f34db68d4becd678d86116",
+    "url": "/quiz/index.html"
+  },
+  {
+    "revision": "df7b6614bf0ddcdef676",
+    "url": "/quiz/static/css/main.e78299a3.chunk.css"
+  },
+  {
+    "revision": "6f8003d83ca1ff5187ac",
+    "url": "/quiz/static/js/2.8242c380.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/quiz/static/js/2.8242c380.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df7b6614bf0ddcdef676",
+    "url": "/quiz/static/js/main.9c83a5b3.chunk.js"
+  },
+  {
+    "revision": "c64fd46a24a4d3f8c1ce",
+    "url": "/quiz/static/js/runtime-main.6ce891c4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/quiz/precache-manifest.8bd18d97a6fd09ff841df24ac580040f.js"
+  "/quiz/precache-manifest.36757325932cb7841dc263ef3e187782.js"
 );
 
 self.addEventListener('message', (event) => {
