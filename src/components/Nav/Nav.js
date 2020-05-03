8f600b2fe1777e7b07c0df2a9e2c83d7cd584494
marklawrence97 <mark_lawrence97@icloud.com>
2020-05-03 11:02:53 +0100

    Add sign out functionality

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,13 +2,20 @@ import React from 'react'
 import './Nav.css'
 
 const Nav = () => {
+
+    const onSignOut = async () => {
+        const response = await fetch('/logout')
+        const content = await response.json()
+        console.log(content)
+    }
+
     return ( 
         <div className="Nav">
             <div className="left-items">
                 Let's get quizzeh
             </div>
             <div className="right-items">
-                <p className="button">Sign Out</p>
+                <p onClick={onSignOut} className="button">Sign Out</p>
             </div>
         </div>
     )
